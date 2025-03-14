19db48f7d9d0aece79d7f474811cc4d64ff3c26b
Nathan Scremin <nathanscremin.dev@gmail.com>
2025-03-14 09:36:17 -0300

    Delete ExA208.py

--- ExA208.py
+++
@@ -1,4 +0,0 @@
-preco_o = 100
-desc = 20
-preco_f = preco_o - (preco_o * (desc/100))
-print('O produto que custava ' + str(preco_o) + ', com desconto de ' + str(desc) + '% ' + 'agora custa ' + str(preco_f))
