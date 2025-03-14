70f0a4e58739067c576737dcbb52ddab88540d69
Nathan Scremin <nathanscremin.dev@gmail.com>
2025-03-14 09:35:34 -0300

    Delete ExA202.py

--- ExA202.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input('Digite o primeiro numero: '))
-num2 = int(input('Digite o segundo numero: '))
-sum = num1 + num2
-print('A soma de ' + str(num1) + ' e ' + str(num2) + ' e igual a ' + str(sum))
