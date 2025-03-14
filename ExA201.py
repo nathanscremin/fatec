a99798b029c4acf7a81e626a22b8f2c259e1a09e
Nathan Scremin <nathanscremin.dev@gmail.com>
2025-03-14 09:35:14 -0300

    Delete ExA201.py

--- ExA201.py
+++
@@ -1,4 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = str(int(input('Digite sua idade: '))) # Coloquei em int por costume, afinal estamos tratando de um numero.
-altura = str(float(input('Digite sua altura (em metros, exemplo 1.80): '))) # Passei para float para ele resumir as casas, por exemplo 1.80 vira 1.8.
-print("Meu nome é " + nome + ", tenho " + idade + " anos e tenho " + altura + " de altura.")
